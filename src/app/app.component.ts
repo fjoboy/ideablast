102aa1a2ee07dacf2965ffe1ee94485ba816dda6
Fabian Johnsen <fjo013uib@gmail.com>
2019-09-09 01:23:48 +0200

    viewport fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { setViewport } from './viewport';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,14 +12,6 @@ export class AppComponent {
 
   constructor(){
 
-    this.setViewport();
-  }
-
-  setViewport(){
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);  
-
+    setViewport();
   }
 }
